a4bcdfbe4e06f6e88bff6e91f17f50ab4cde4582
Aaron Bailey <aaronthesnowboarder@live.com>
2022-06-09 14:14:25 -0700

    added ground travel guide

--- src/Pages/TravelPage/TravelPage.tsx
+++ src/Pages/TravelPage/TravelPage.tsx
@@ -21,7 +21,7 @@ const TravelPage:FC=()=>{
                         <Col>
                             <Card className='travelCard'>
                                 <h3 className='travelSubHeader'>Seattle Tacoma International Airport</h3>
-                                <img src={require('../../assets/SEA.jpg')} className='cardImg'/>
+                                <img src={require('../../assets/SEA.jpg')} className='cardImg' alt= ' Seattle Tacoma International from the air'/>
                                 <div className='travelText'>
                                 Seattle's main airport! Serving as the cities internation airport, "Sea-Tac" provided routes from all around the world! Most major US arilines go here, and many internation Airlines.
                                 </div>
@@ -31,9 +31,34 @@ const TravelPage:FC=()=>{
                         <Col>
                             <Card className='travelCard'>
                                 <h3 className='travelSubHeader'>Paine Field</h3>
-                                <img src={require('../../assets/Paine_Field.jpg')} className='cardImg'/>
+                                <img src={require('../../assets/Paine_Field.jpg')} className='cardImg' alt='Paine Field from the air'/>
                                 <div className='travelText'>
                                 Paine Field serves the north of Seattle with limited service. Mainly served by Alaska Airlines, with destinations from/to Boise Las Vegas, LA, Phoenix, San Francisco! Not as well contected as Sea-Tac, but you can find some good deals!
+                                </div>
+                            </Card>
+                        
+                        </Col>
+                    </Row>
+                </div>
+                <div>
+                <h2 className='TravelCatHeader'>Ground Travel</h2>
+                <Row>
+                        <Col>
+                            <Card className='travelCard'>
+                                <h3 className='travelSubHeader'>Amtrak</h3>
+                                <img src={require('../../assets/amtrak.jpg')} className='cardImg' alt='Amtrak Train in the snow'/>
+                                <div className='travelText'>
+                                    Amtrak is a major rail service in the United States. In Seattle, Amtrak goes to King Street Station! Taking Amtrak to Seattle is a great way to see the Pacific Northwest! May not be has fast as a plane, but you wil lbe treated to amazing views!
+                                </div>
+                            </Card>
+                        
+                        </Col>
+                        <Col>
+                        <Card className='travelCard'>
+                                <h3 className='travelSubHeader'>Interstates</h3>
+                                <img src={require('../../assets/I90.jpg')} className='cardImg' alt='I-90 goive over Lake Washingtion'/>
+                                <div className='travelText'>
+                                    Want to travel by car? Well your in luck! Seattle is easy to drive to by Interstate! With I-5 connecting the city all the way south to LA and all the way north to Vancouver Canada. As well ass I-90 connecting the city all the way to Boston!
                                 </div>
                             </Card>
                         
