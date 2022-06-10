20a2339d1a22b26bc90bd1a9259e7868aac55831
Aaron Bailey <aaronthesnowboarder@live.com>
2022-06-09 21:06:22 -0700

    added demo popout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import { BrowserRouter as Router} from 'react-router-dom'
 import CustomRouter from './CustomRouter';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import './App.scss';
 import NavBarComp from './Components/NavBar/NavBarComp';
 import './MainTheme.scss'
 
@@ -14,6 +14,14 @@ const App: FC=()=>{
        <NavBarComp/>
        <Router>
           <CustomRouter/>
+          <div className='popout'>
+            <div className='mainpopout'>
+              <h2 className='popoutTitle'>This is a demo website</h2>
+              <button className='closedPopoutBtn'>X</button>
+              <p>This webshite is made by Aaron Bailey as a demo website project. Checkout the code here:<a href='https://github.com/AaronB22/CompanyPageProj' >https://github.com/AaronB22/CompanyPageProj</a></p>
+            </div>
+
+            </div>
        </Router>
       </>
     )
